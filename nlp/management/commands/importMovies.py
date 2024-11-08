79fd2a04d0706a37c13d263faf592b8ec1fcc156
EthanCue <cuellar.reyes.ethan@gmail.com>
2024-11-08 10:59:59 -0600

    works with de db

--- nlp/management/commands/importMovies.py
+++ nlp/management/commands/importMovies.py
@@ -1,6 +1,9 @@
+import logging
 import pandas as pd
 from django.core.management.base import BaseCommand
 from ...models import Movie
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Import movies from Excel'
@@ -12,19 +15,27 @@ class Command(BaseCommand):
         # self.stdout.write(self.style.WARNING('Todos los registros de películas han sido eliminados'))
         
         # Lee el archivo de Excel
-        df = pd.read_excel('C:/Users/ethan/OneDrive/Desktop/ESCOM/ESCOM_6/Ingenieria_de_Software/FilmFind/film_dataset/moviesShortClean.xlsx')
+        df = pd.read_excel('./film_dataset/moviesClean.xlsx')
 
         for _, row in df.iterrows():
-            Movie.objects.update_or_create(
-                title=row['title'],  # Campo clave para identificar la película
-                defaults={
-                    'overview': row.get('overview', ''),
-                    'poster_path': row.get('poster_path', ''),
-                    'genres': row.get('genres', ''),
-                    'original_language': row.get('original_language', ''),
-                    'release_date': row.get('release_date', None),
-                    'tagline': row.get('tagline', ''),
-                    'keywords': row.get('keywords', '')
-                }
-            )
+            try:
+                title = row['title']
+                if len(title) > 255:
+                    title = title[:255]
+                Movie.objects.update_or_create(
+                    defaults={
+                        'overview': row.get('overview', ''),
+                        'poster_path': row.get('poster_path', ''),
+                        'genres': row.get('genres', ''),
+                        'original_language': row.get('original_language', ''),
+                        'tagline': row.get('tagline', ''),
+                        'keywords': row.get('keywords', ''),
+                        'normalizedOverview': row.get('normalizedOverview', '')
+                    }
+                )
+            except Exception as e:
+                # Si ocurre un error, registra el error y los datos que causaron el problema
+                logger.error(f"Error al procesar la película con título '{row['title']}': {e}")
+                logger.error(f"Datos que causaron el error: {row}")
+                continue
         self.stdout.write(self.style.SUCCESS('Datos importados correctamente'))
