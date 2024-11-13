40b7b18de825e3ac4fed4635a54bedc959f0759e
EthanCue <cuellar.reyes.ethan@gmail.com>
2024-11-13 17:27:26 -0600

    idk

--- nlp/models.py
+++ nlp/models.py
@@ -7,9 +7,9 @@ class Movie(models.Model):
     overview = models.TextField(blank=True)
     genres = models.TextField(max_length=255)
     original_language = models.CharField(max_length=10)
-    release_date = models.DateField(null=True, blank=True)
+    release_date = models.DateField(blank=True)
     tagline = models.CharField(max_length=255, blank=True)
-    keywords = models.TextField(null=True, blank=True)
+    keywords = models.TextField(blank=True)
     poster_path = models.URLField(max_length=500, blank=True)
     normalizedOverview = models.TextField(blank=True)
 
