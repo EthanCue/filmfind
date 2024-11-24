daf3437a52e622441200512555a6258628fe7db9
EthanCue <cuellar.reyes.ethan@gmail.com>
2024-11-23 21:30:19 -0600

    admin y apps

--- nlp/admin.py
+++ nlp/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
-#from .models import Movie
-
-# Register your models here.
-#admin.site.register(Movie)
-=======
 from .models import Movie
 
 # Register your models here.
 admin.site.register(Movie)
->>>>>>> tests
